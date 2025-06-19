d328556f2868f5cf8a78437a403db67d96bb3084
vaishupadmor <vaishupadmor@gmail.com>
2025-06-19 15:13:50 +0530

    code-added

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app =express();
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000","https://final-project-rtc.onrender.com"],
       credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"],
        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
